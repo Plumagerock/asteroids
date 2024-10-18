e279a72589261ac683b9b83186b1564d28b9db7c
plumagerock <plumagerock@gmail.com>
2024-10-18 01:42:08 -0400

    Add files

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from constants import *
 
 
 def main():
-    print(f"Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill(000)
 
 
 if __name__ == "__main__":
