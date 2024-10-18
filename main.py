8238c59609cc4791be3a1ed7b319a22337275509
plumagerock <plumagerock@gmail.com>
2024-10-18 02:11:49 -0400

    Add Clock.tick

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +17,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        
+        # limit framerate to 60
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
