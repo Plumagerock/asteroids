d25449b4064edf43f3416bd59a8ef6acfaa53e19
Plumagerock <plumagerock@gmail.com>
2024-10-19 13:29:52 -0400

    Add circleshape and Player. define player to visual triangle.

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ from constants import *
 
 
 def main():
-    print(f"Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+        
+        # limit framerate to 60
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
