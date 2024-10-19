4aec206baee8043d9cf45006ed8db159446c3caa
Plumagerock <plumagerock@gmail.com>
2024-10-19 13:59:30 -0400

    Update main

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 1)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     dt = 0
 
@@ -17,10 +17,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        # fill scrren with black
         screen.fill("black")
-        pygame.display.flip()
 
+        # draw player to screen
         player.draw(screen)
+
+        # update screen
+        pygame.display.flip()
         
         # limit framerate to 60
         dt = clock.tick(60) / 1000
